677f6079e510ca7af0d9ff74a92e3c90da2d9b4f
oddtomas <oddthomas@csu.fullerton.edu>
2021-08-07 17:44:42 -0700

    finished 2/3 conding challenges

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -17,33 +17,64 @@
 
 // a) Create a test with an expect statement using the variables provided.
 
+describe("codeWord", () => {
 var secretCodeWord1 = "Lackadaisical"
 // Expected output: "L4ck4d41s1c4l"
 var secretCodeWord2 = "Gobbledygook"
 // Expected output: "G0bbl3dyg00k"
 var secretCodeWord3 = "Eccentric"
 // Expected output: "3cc3ntr1c"
+    it("takes in a string and returns a coded message. The coded message converts 'a' to 4, 'e' to 3, 'i' to 1, and 'o' to 0.", () => {
+      expect(codeWord(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+      expect(codeWord(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+      expect(codeWord(secretCodeWord3)).toEqual("3cc3ntr1c")
 
+    })
+  })
 
 
 // b) Create the function that makes the test pass.
+//create a function that takes a string
+codeWord = (str) => {
+    //create an empty string to save the conversion
+    var newStr = ""
+    //make an object representing character mappings to be used with the replace function. ('a' swaps to '4', 'e' swaps to '3' so on so forth hedging for capitol letters)
+    var chars = {'a':'4','A':'4','e':'3','E':'3','i':'1','I':'1','o':'0','O':'0'};
+    //use replace with a function as the argument that gets called for each match 
+    newStr = str.replace(/[aeioAEIO]/g, m => chars[m]); //[aeioAEIO] are the characters being checked for "g" represents all instances of these characters. "m => chars[m]" is our function for swapping the characters, where there would normally be another argument for replace(). Replace() works with the parameters of (old thing, new thing) usually. 
+
+//return the new coded string    
+return newStr 
 
 
+    }
 
 // --------------------2) Create a function that takes in an array and returns all the words that contain the letter a.
 
 // a) Create a test with an expect statement using the variable provided.
 
-var arrayOfWords1 = ["Apple", "Banana", "Plum", "Orange", "Kiwi"]
-// Expected output: ["Apple", "Banana", "Orange"]
-var arrayOfWords2 = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach"]
-// Expected output: ["Mango", "Apricot", "Peach"]
-
+describe("aWords", () => {
+    var arrayOfWords1 = ["Apple", "Banana", "Plum", "Orange", "Kiwi"]
+    // Expected output: ["Apple", "Banana", "Orange"]
+    var arrayOfWords2 = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach"]
+    // Expected output: ["Mango", "Apricot", "Peach"]
+        it("takes in an array and returns all the words that contain the letter a", () => {
+          expect(aWords(arrayOfWords1)).toEqual(["Apple", "Banana", "Orange"])
+          expect(aWords(arrayOfWords2)).toEqual(["Mango", "Apricot", "Peach"])  
+        })
+      })
 
 
 // b) Create the function that makes the test pass.
 
+//create a function that takes an array   
+aWords = (arr) => {
+    //iterate through the array to find words that include 'a' or 'A'
+    var result = arr.filter(item => item.includes('a') || item.includes('A')); //using filter go through the array with the conditions of include('a') or include('A')
 
+   //return an array of all the words that contain the letter a 
+return result
+}
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is exactly one pair and one three of a kind.
 
